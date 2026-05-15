243cfe59b7453bcdcbc7039b28bf1fe825fae5a6
shawonstack <shawon934764@gmail.com>
2026-05-15 22:39:10 +0600

    optional chaining

--- day3.js
+++ day3.js
@@ -1,45 +1,55 @@
-let num1 = 3.3;
-let num2 = 55;
-let res = num1 % num2;
-console.log(res);
+// let num1 = 3.3;
+// let num2 = 55;
+// let res = num1 % num2;
+// console.log(res);
 
-let num3 = 15;
-num3 *= 4;
-console.log(num3);
-let food = 50;
-food /= 10;
-console.log(food);
-let age = 18;
-let result1 = age >= 20 ? 'adult' : 'minor';
+// let num3 = 15;
+// num3 *= 4;
+// console.log(num3);
+// let food = 50;
+// food /= 10;
+// console.log(food);
+// let age = 18;
+// let result1 = age >= 20 ? 'adult' : 'minor';
+// // console.log(result);
+
+// const price = 400;
+// const result = price <= 2000 ? 'easy to bye' : 'impossible';
 // console.log(result);
 
-const price = 400;
-const result = price <= 2000 ? 'easy to bye' : 'impossible';
-console.log(result);
+// let age2 = 16;
+// let res2 = age2 >= 18 ? 'adult' : 'teen';
+// console.log(res2);
 
-let age2 = 16;
-let res2 = age2 >= 18 ? 'adult' : 'teen';
-console.log(res2);
+// let nums = 25;
+// let resA = nums % 2 == 0 ? 'even' : 'odd';
+// console.log(resA);
+// let temperature = 35;
+// let resB = temperature >= 30 ? 'hot' : 'normal';
+// console.log(resB);
 
-let nums = 25;
-let resA = nums % 2 == 0 ? 'even' : 'odd';
-console.log(resA);
-let temperature = 35;
-let resB = temperature >= 30 ? 'hot' : 'normal';
-console.log(resB);
+// let isRain = false;
+// let resC = isRain == true ? 'take umbrella' : 'no need ';
+// console.log(resC);
+// let marks = 28;
+// let resD = marks >= 33 ? 'pass' : 'fail';
+// console.log(resD);
+// let username = 'blood';
+// let resE = username == 'admin' ? 'admin access' : 'user access';
+// console.log(resE);
+// let money = 500;
+// let resF = money >= 1000 ? 'bue phone' : 'save money';
+// console.log(resF);
+// let language = 'js';
+// let resG = language == 'js' ? 'javascript' : 'unknown';
+// console.log(resG);
 
-let isRain = false;
-let resC = isRain == true ? 'take umbrella' : 'no need ';
-console.log(resC);
-let marks = 28;
-let resD = marks >= 33 ? 'pass' : 'fail';
-console.log(resD);
-let username = 'blood';
-let resE = username == 'admin' ? 'admin access' : 'user access';
-console.log(resE);
-let money = 500;
-let resF = money >= 1000 ? 'bue phone' : 'save money';
-console.log(resF);
-let language = 'js';
-let resG = language == 'js' ? 'javascript' : 'unknown';
-console.log(resG);
+let A = 0;
+// console.log(A || 'not name');
+
+let bio = {
+  name: 'shaon',
+  age: 33,
+  address: { city: 'dhaka' },
+};
+console.log(bio.address?.city);
