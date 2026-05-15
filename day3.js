9c2f9a1732733c50151c32cd8bccd86859786389
shawonstack <shawon934764@gmail.com>
2026-05-15 12:12:18 +0600

    ternary operator usecase

--- day3.js
+++ day3.js
@@ -9,3 +9,28 @@ console.log(num3);
 let food = 50;
 food /= 10;
 console.log(food);
+let age = 18;
+let result1 = age >= 20 ? 'adult' : 'minor';
+// console.log(result);
+
+const price = 400;
+const result = price <= 2000 ? 'easy to bye' : 'impossible';
+console.log(result);
+
+let age2 = 16;
+let res2 = age2 >= 18 ? 'adult' : 'teen';
+console.log(res2);
+
+let nums = 25;
+let resA = nums % 2 == 0 ? 'even' : 'odd';
+console.log(resA);
+let temperature = 35;
+let resB = temperature >= 30 ? 'hot' : 'normal';
+console.log(resB);
+
+let isRain = false;
+let resC = isRain == true ? 'take umbrella' : 'no need ';
+console.log(resC);
+let marks = 28;
+let resD = marks >= 33 ? 'pass' : 'fail';
+console.log(resD);
